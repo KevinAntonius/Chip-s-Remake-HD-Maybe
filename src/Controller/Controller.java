095dcb0c2469dba7b31d47953bdbf729cd39e5f7
Steven-Haryanto <the_melting.chocolate@yahoo.com>
2014-11-22 23:37:47 +0700

    Penyempurnaan grafik dan controller

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -17,14 +17,64 @@ import java.util.logging.Logger;
  * @author STEVEN
  */
 public class Controller implements KeyListener{
+    /**
+     * Chip yang menjadi pemain utama dalam permainan ini dan dikontrol oleh user.
+     */
     private Chip chip;
+    
+    /**
+     * Dunia yang Chip sekarang berada. 
+     */
     private World world; 
-    private GameObject object;
+    
+    
+    /**
+     * Kumpulan Map untuk diimplementasikan ke World
+     */
     private MapIterable maps;
+    
+    /**
+     * Atribut untuk menandakan bahwa tidak ada map lagi yang tersedia
+     */
     private boolean gameFinish;
+    
+    /**
+     * Atribut untuk menandakan bahwa level di map sekarang sudah selesai
+     */
+    private boolean finishLevel;
+    
+    /**
+     * Path untuk mengakses ke map
+     */
     private String path;
-    private WorldViewer chips;
+    
+    /**
+     * Viewer interface untuk dunia dan chip sehingga user dapat melihat apa yang terjadi di world dan chip
+     */
+    private WorldViewer worldViewer;
+    
+    /**
+     * Isi map yang sudah diimplementasikan ke world dalam bentuk kode
+     */
     private String isiMap;
+    
+    
+    /**
+     * Metod untuk menyuruh chip untuk bergerak. Chip tidak bisa bergerak ke arah 
+     * tersebut jika ada tembok atau barrier. Barrier akan hancur jika semua IC 
+     * sudah terkumpul dan chip bergerak ke arah barrier. Lalu di metod ini juga
+     * menangani jika chip bergerak ke arah trap ,seperti FireFloor atau Pool, 
+     * menangani pengambilan item IC dan juga Shoes, seperti RedShoes atau BlueShoes.
+     * Menangani juga jika chip berada di Finish.
+     * @param direction angka untuk Chip bergerak.
+     * <ul>
+     *      <li>2 untuk bergerak ke bawah</li>
+     *       <li>8 untuk bergerak ke atas</li>
+     *       <li>4 untuk bergerak ke kiri</li>
+     *       <li>6 untuk bergerak ke kanan</li>
+     * </ul>
+     * @throws IOException 
+     */
     public void chipMove(int direction) throws IOException{
         int x=chip.getX();
         int y=chip.getY();
@@ -38,6 +88,10 @@ public class Controller implements KeyListener{
             x+=1;
         }
         if(!this.wallCheck(x,y)){
+            GameObject go=this.world.getObjectAt(x, y);
+            if((this.world.getKodeMapAt(this.chip.getX(), this.chip.getY()).equalsIgnoreCase("f")||this.world.getKodeMapAt(this.chip.getX(), this.chip.getY()).equalsIgnoreCase("p"))&&(this.world.getKodeMapAt(x, y).equalsIgnoreCase("n")||this.world.getKodeMapAt(x, y).equalsIgnoreCase("i")||this.world.getKodeMapAt(x, y).equalsIgnoreCase("m")||this.world.getKodeMapAt(x, y).equalsIgnoreCase("r"))){
+                this.chip.setToNormalWalker();
+            }
             if(direction == 2){
                 chip.moveDown();
             }else if(direction == 4){
@@ -47,34 +101,29 @@ public class Controller implements KeyListener{
             }else if(direction == 8){
                 chip.moveUp();
             }
-            //System.out.println(this.chip.getX()+" "+this.chip.getY());
-            //System.out.println(this.getGameObjectAt(this.chip.getX(), this.chip.getY()).getName());
-       
-           //chip.move(x,y);
-           this.deathCheck();
-           this.itemCheck();
-           if(this.finishCheck()){
-               Map newMap=this.nextLevel();
-               if(newMap==null){
-                   this.gameFinish=true;
-               }
-               else{
-                  this.implementsMapToWorld(newMap);
-                  this.chips.fillContent();
-                  this.chips.moved();
-               }
+           if(this.deathCheck(direction)){
            }
+           this.itemCheck();
            
+           if(this.finishCheck()){
+               this.finishLevel=true;
+           }
        }
        else{
            if(this.tryOpenWallOrBarrier(x, y)){
                this.world.destroyObjectAt(x, y);
-               this.chips.afterTaken(x,y);
+               this.worldViewer.afterTaken(x,y);
                
            }
        }
     }
     
+    /**
+     * Metod untuk mengetahui apakah di x dan y ada objek bernama Wall atau Barrier
+     * @param x baris
+     * @param y kolom
+     * @return benar jika di baris x dan y ada Wall atau Barrier.
+     */
     private boolean wallCheck(int x, int y){
         if(world.getObjectAt(x,y)==null){
             return false;
@@ -82,40 +131,64 @@ public class Controller implements KeyListener{
         return (world.getObjectAt(x,y).getName().equalsIgnoreCase("Wall")||world.getObjectAt(x, y).getName().equalsIgnoreCase("Barrier"));
     }
     
-    private boolean deathCheck(){
+    /**
+     * Metod untuk mengecek apakah chip meninggal atau tidak. jika Chip berada di trap FireFloor atau pool tanpa memiliki sepatu yang diperlukan, maka Chip di set meninggal. Tidak jika sebaliknya.
+     * @return true jika chip meninggal. Tidak jika chip memang tidak meninggal atau letak chip sekarang bukan trap.
+     */
+    private boolean deathCheck(int direction){
         GameObject go =  world.getObjectAt(chip.getX(), chip.getY());
+        boolean isDead=false;
         if(go!=null){
              if(go.getName().equalsIgnoreCase("Fire Floor")||go.getName().equalsIgnoreCase("Pool")){
                 Traps trap=(Traps) go;
                 if(!chip.shoesCheck(trap.getRequirementShoes())){
                 chip.isDead();
-                }  
-                return chip.getDead();
-            }
+                }
+                else{
+                    if(go.getName().equalsIgnoreCase("Fire Floor")){
+                        this.chip.setToLavaWalker();
+                    }
+                    else if(go.getName().equalsIgnoreCase("Pool")){
+                        this.chip.setToWaterWalker();
+                    }
+                }
+                isDead= chip.getDead();
+            }         
         }
+        
             return false;
     }
+    
+    /**
+     * Metod untuk mengecek objek yang sekarang Chip tempati merupakan objek yang
+     * bernama Blue Shoes, Red Shoes, atau IC. Jika iya, maka objek tsb akan diambil 
+     * oleh chip dan lalu menghancurkan objek tsb di world.
+     * @throws IOException 
+     */
     private void itemCheck() throws IOException{
         GameObject go = world.getObjectAt(chip.getX(),chip.getY());
         if(go!=null){
             if(go.getName().equalsIgnoreCase("Blue Shoes")){
                 this.chip.getShoes((Shoes)go);
                 this.world.destroyObjectAt(chip.getX(),chip.getY());
-                this.chips.afterTaken(chip.getX(),chip.getY());
+                this.worldViewer.afterTaken(chip.getX(),chip.getY());
             }else if(go.getName().equalsIgnoreCase("Red Shoes")){
                 this.chip.getShoes((Shoes)go);
                 this.world.destroyObjectAt(chip.getX(),chip.getY());
-                this.chips.afterTaken(chip.getX(),chip.getY());
+                this.worldViewer.afterTaken(chip.getX(),chip.getY());
             }else if(go.getName().equalsIgnoreCase("IC")){
-                //pikirin lagi
                 IC ic=(IC)go;
                 ic.getIC();
                 this.world.destroyObjectAt(chip.getX(),chip.getY());
-                this.chips.afterTaken(chip.getX(),chip.getY());
+                this.worldViewer.afterTaken(chip.getX(),chip.getY());
             }
         }   
     }
     
+    /**
+     * Metod untuk mengecek apakah objek ditempati chip bernama Finish
+     * @return true jika objek objek yang ditempati chip bernama Finish, false jika sebaliknya
+     */
     private boolean finishCheck(){
         GameObject go = world.getObjectAt(chip.getX(),chip.getY());
         if(go!=null){
@@ -126,12 +199,22 @@ public class Controller implements KeyListener{
         return false;
     }
     
-    
+    /**
+     * Metod untuk membuka wall atau barrier di baris x dan y
+     * @param x baris
+     * @param y kolom
+     * @return true jika barrier dan semua IC terkumpul, false jika wall atau barrier dan semua IC tidak terkumpul.
+     */
     private boolean tryOpenWallOrBarrier(int x, int y){
         Wall barrier = (Wall) world.getObjectAt(x,y);
         return barrier.tryOpen();
     }
     
+    /**
+     * Metod untuk menginisialisasi kumpulan map melalui parameter path dan lalu memulai game dengan map pertama
+     * @param path 
+     * @return Map level perama
+     */
     public Map start(String path){
         this.path=path;
         maps=new Level(path);
@@ -139,11 +222,20 @@ public class Controller implements KeyListener{
         return (Map)mapi.next();
     }
     
+    /**
+     * Metod untuk melanjutkan map dengan level selanjutnya
+     * @return map dengan leve selanjutnya
+     */
     private Map nextLevel(){
         return (Map)this.maps.getIterator().next();
     }
     
+   /**
+    * Metod untuk mengimplementasikan kode di map menjadi ke World
+    * @param newMap kode map
+    */
    public void implementsMapToWorld(Map newMap){
+       this.finishLevel=false;
        String str=newMap.getIsiMap();
        this.isiMap=str.substring(6);
        String []split=str.split("\n");
@@ -198,14 +290,19 @@ public class Controller implements KeyListener{
        }
    }
    
-   public GameObject getGameObjectAt(int x, int y){
-       return this.world.getObjectAt(x, y);
-   }
    
+   /**
+    * Metod untuk mendapatkan Chip yang sekarang di kontrol oleh user
+    * @return chip Chip yang sekarang di kontrol oleh user
+    */
    public Chip getChip(){
        return this.chip;
    }
    
+   /**
+    * Metod untuk mendapatkan Chip
+    * @return 
+    */
    public URL sendURLChip(){
        return this.chip.sendCurrentURL();
    }
@@ -221,13 +318,16 @@ public class Controller implements KeyListener{
         return this.world.sendURLAtObject(x, y);
     }
     
-
+    /**
+     * Metod untuk mengembalikan atribut gameFinish
+     * @return 
+     */
     public boolean getGameFinish(){
         return this.gameFinish;
     }
 
     /**
-     * 
+     * Metod untuk mengembalikan atribu world
      * @return 
      */
     public World getWorld(){
@@ -239,27 +339,62 @@ public class Controller implements KeyListener{
         
     }
 
+    /**
+     * Metod yang meng-override keyPressed.
+     * @param e 
+     */
     @Override
     public void keyPressed(KeyEvent e) {
-        int direction = -1;
-        if(e.getKeyCode() == KeyEvent.VK_UP){
-            direction = 8;
+        if(!this.chip.getDead()&&!this.finishLevel){
+            int direction = -1;
+            if(e.getKeyCode() == KeyEvent.VK_UP||e.getKeyCode() == KeyEvent.VK_W){
+                direction = 8;
+            }
+            else if(e.getKeyCode() == KeyEvent.VK_LEFT||e.getKeyCode() == KeyEvent.VK_A){
+                direction = 4;
+            }
+            else if(e.getKeyCode() == KeyEvent.VK_RIGHT||e.getKeyCode() == KeyEvent.VK_D){
+                direction = 6;
+            }
+            else if(e.getKeyCode() == KeyEvent.VK_DOWN||e.getKeyCode() == KeyEvent.VK_S){
+                direction = 2;
+            }
+            try {
+                this.chipMove(direction);
+            } catch (IOException ex) {
+                Logger.getLogger(Controller.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            }
+            this.worldViewer.moved();
         }
-        else if(e.getKeyCode() == KeyEvent.VK_LEFT){
-            direction = 4;
+        else if(this.finishLevel){
+            if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE){
+                Map newMap=this.nextLevel();
+               if(newMap==null){
+                   this.gameFinish=true;
+               }
+               else{
+                  this.implementsMapToWorld(newMap);
+                    try {
+                        this.worldViewer.fillContent();
+                    } catch (IOException ex) {
+                        Logger.getLogger(Controller.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+                    }
+                  this.worldViewer.moved();
+               }
+            }
         }
-        else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
-            direction = 6;
+        if(e.getKeyCode() == KeyEvent.VK_SPACE){
+                this.implementsMapToWorld(this.restart());
+            try {
+                this.worldViewer.fillContent();
+                this.worldViewer.repaint();
+            } catch (IOException ex) {
+                Logger.getLogger(Controller.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            }
+            }
+        else if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
+            System.exit(0);
         }
-        else if(e.getKeyCode() == KeyEvent.VK_DOWN){
-            direction = 2;
-        }
-        try {
-            this.chipMove(direction);
-        } catch (IOException ex) {
-            Logger.getLogger(Controller.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        this.chips.moved();
     }
 
     @Override
@@ -267,16 +402,32 @@ public class Controller implements KeyListener{
         
     }
     
+    /**
+     * Metod untuk merestart map. Metod ini juga mengembalikan banyak IC menjadi 0.
+     * @return Map lvl pertama
+     */
     public Map restart(){
+        IC ic=new IC();
+        while(IC.totalChip!=0){
+            ic.getIC();
+        }
         maps=new Level(path);
         MapIterator mapi=maps.newIterator();
         return (Map)mapi.next();
     }
     
+    /**
+     * Metod untuk mengeset worldViewer menjadi worldV
+     * @param worldV WorldViewer GUI yang sekarang muncul
+     */
     public void implementsWorldViewer(WorldViewer worldV){
-        this.chips = worldV;
+        this.worldViewer = worldV;
     }
     
+    /**
+     * Metod untuk mengembalikan kumpulan string yang berisi kode dari tiap GameObjeck yang berbeda kelas
+     * @return kumpulan string yang berisi kode dari tiap GameObjeck yang berbeda kelas
+     */
     public String kodeTipeGameObjekDiMapSekarang(){
         String str="";
         String[][] arrayMap=this.world.getIsiMap();
@@ -297,6 +448,12 @@ public class Controller implements KeyListener{
         return str;
     }
     
+    /**
+     * metod untuk mengecheck apakah kode di newString ada di kumpulan String str
+     * @param str kumpulan String
+     * @param newString kode GameObject
+     * @return true jika ada, false jika tidak ada
+     */
     private boolean checkExistChar(String str, String newString){
         boolean exist=false;
         for(int i=0;i<str.length()&&!exist;i++){
@@ -357,7 +514,14 @@ public class Controller implements KeyListener{
         return result;
     }
     
+    /**
+     * Metod untuk memanggil metod di world bernama getKodeMapAt(int x, int y)
+     * @param x baris
+     * @param y kolom
+     * @return kode map dari baris x dan kolom y
+     */
     public String getKodeMapAt(int x, int y){
         return this.world.getKodeMapAt(x, y);
+        
     }
 }
